7ba6f8bd3bbeed774453322990f9f7cbc81b23ff
Guilherme Miyata <miyatagui120600@gmail.com>
2025-11-13 00:17:55 -0300

    feat: adiciona contrller.html ao script

--- interface/frontend/nav-menu.js
+++ interface/frontend/nav-menu.js
@@ -7,6 +7,7 @@ const NAV_ITEMS = [
   { url: 'index.html', icon: 'home', label: 'Início' },
   { url: 'actuators.html', icon: 'videogame_asset', label: 'Controle' },
   { url: 'kinematics.html', icon: 'straighten', label: 'Cinemática' },
+  { url: 'controller.html', icon: 'sports_esports', label: 'Joystick' },
   { url: 'routines.html', icon: 'autorenew', label: 'Rotinas' },
   { url: 'motion.html', icon: 'gps_fixed', label: 'MPU-6050' },
   { url: 'settings.html', icon: 'settings', label: 'Configurações' },
